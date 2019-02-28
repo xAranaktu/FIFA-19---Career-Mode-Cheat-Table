9ac0e039a35cfbf465fcf968fd6993073f4ef41d
xAranaktu <paweh10@gmail.com>
2019-02-28 18:58:27 +0100

    update create_dirs

--- FIFA 19 - CM Cheat Table/create_dirs.bat
+++ FIFA 19 - CM Cheat Table/create_dirs.bat
@@ -1,8 +1,10 @@
 @echo off
 ECHO Creating Cheat Table folder in Documents\FIFA 19
-SET cache_path=%HOMEDRIVE%\Users\%USERNAME%\Documents\FIFA 19\Cheat Table\cache\
+SET ct_path=%HOMEDRIVE%\Users\%USERNAME%\Documents\FIFA 19\Cheat Table\
+SET cache_path=%ct_path%cache\
 md "%cache_path%crest"
 md "%cache_path%heads"
 md "%cache_path%youthheads"
+md "%ct_path%data"
 copy tmp\crest_notfound.png "%cache_path%crest\notfound.png" 
 copy tmp\heads_notfound.png "%cache_path%heads\notfound.png"
